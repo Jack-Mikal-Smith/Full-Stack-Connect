d3751b25e6328899bed898c40223e912e461f8f2
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-06-05 19:24:40 -0500

    added tabs, main handlebars

--- server.js
+++ server.js
@@ -3,6 +3,32 @@ const controllers = require('./app/controllers'); // Assuming index.js is the de
 
 const app = express();
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
 // Define routes
 console.log(controllers); // Log the controllers object to check its contents
 app.use(express.json());
