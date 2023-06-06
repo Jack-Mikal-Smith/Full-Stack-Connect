48e292a5792ec33a0cf6bcc22470e3684c7f3604
StrugglerXIV <arbiterdenis@gmail.com>
2023-06-05 21:42:49 -0400

    Sign in button works

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,19 +1,39 @@
-const path = require('path');
+const User = require('../models/User');
 
 const HomeController = {
   renderSignIn: (req, res) => {
-    res.render('layouts/signin', { layout: false }); // Render the signin view with handlebars
+    res.render('layouts/signin', { layout: false, error: null });
   },
-  signIn: (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log(email);
-    console.log(password);
-    // Process the form submission and handle authentication logic here
 
-    // Redirect or respond based on the result
-    res.redirect('/'); // Example redirect to the dashboard page
-  }
+  signIn: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      // Find the user by email
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        // User not found
+        return res.render('layouts/index', { layout: false, error: 'Invalid email or password' });
+      }
+
+      // Verify password
+      const isValidPassword = user.checkPassword(password);
+      if (!isValidPassword) {
+        // Invalid password
+        return res.render('layouts/index', { layout: false, error: 'Invalid email or password' });
+      }
+
+      // Store user information in session
+      req.session.user = { id: user.id, email: user.email };
+
+      // Redirect to authenticated area or any other desired location
+      res.redirect('/users');
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.render('layouts/main', { layout: false, error: 'An error occurred during login' });
+    }
+  },
 };
 
 module.exports = HomeController;
