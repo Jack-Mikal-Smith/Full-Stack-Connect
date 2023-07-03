8886532793d6ce5b8c2ade08d96d69bcf5fe3447
StrugglerXIV <arbiterdenis@gmail.com>
2023-07-02 20:29:34 -0400

    Job Posting now has full functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const jobPostingRoutes = require("./api/job-posting-routes");
 const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
+router.use("/JobPostings", jobPostingRoutes);
 
 
 
